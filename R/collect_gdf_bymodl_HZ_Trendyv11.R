754cf63ab1a42ed1781f706a0210899d07ac07b8
Beni Stocker <benjamin.stocker@gmail.com>
2025-08-13 15:50:03 +0200

    merged and resolved all problems

--- R/collect_gdf_bymodl_HZ_Trendyv11.R
+++ R/collect_gdf_bymodl_HZ_Trendyv11.R
@@ -28,7 +28,7 @@ collect_gdf_bymodl <- function(
       by = c("lon", "lat")
     ) %>%
 
-    ## cVeg
+    # cVeg
     left_join(
       read_nc_onefile(paste0(dir, "/data/", filn_cveg_init, ".nc"), ignore_time = TRUE, varnam = "cVeg") %>%
         nc_to_df(varnam = "cVeg") %>%
@@ -44,61 +44,55 @@ collect_gdf_bymodl <- function(
       by = c("lon", "lat")
     ) %>%
 
-    ## cLeaf
+    # cLeaf
     left_join(
-      read_nc_onefile(paste0(dir, "/data/", filn_cleaf_init, ".nc"), ignore_time = TRUE, varnam = "cLeaf") %>%
-        nc_to_df(varnam = "cLeaf") %>%
+      my_read_nc_onefile(dir, filn_cleaf_init, "cLeaf") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(cleaf_init = myvar),
       by = c("lon", "lat")
     ) %>%
     left_join(
-      read_nc_onefile(paste0(dir, "/data/", filn_cleaf_final, ".nc"), ignore_time = TRUE, varnam = "cLeaf") %>%
-        nc_to_df(varnam = "cLeaf") %>%
+      my_read_nc_onefile(dir, filn_cleaf_final, "cLeaf") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(cleaf_final = myvar),
       by = c("lon", "lat")
     ) %>%
 
-    ## cRoot
+    # cRoot
     left_join(
-      read_nc_onefile(paste0(dir, "/data/", filn_croot_init, ".nc"), ignore_time = TRUE, varnam = "cRoot") %>%
-        nc_to_df(varnam = "cRoot") %>%
+      my_read_nc_onefile(dir, filn_croot_init, "cRoot") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(croot_init = myvar),
       by = c("lon", "lat")
     ) %>%
     left_join(
-      read_nc_onefile(paste0(dir, "/data/", filn_croot_final, ".nc"), ignore_time = TRUE, varnam = "cRoot") %>%
-        nc_to_df(varnam = "cRoot") %>%
+      my_read_nc_onefile(dir, filn_croot_final, "cRoot") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(croot_final = myvar),
       by = c("lon", "lat")
     ) %>%
 
-    ## cWood
+    # cWood
     left_join(
-      read_nc_onefile(paste0(dir, "/data/", filn_cwood_init, ".nc"), ignore_time = TRUE, varnam = "cWood") %>%
-        nc_to_df(varnam = "cWood") %>%
+      my_read_nc_onefile(dir, filn_cwood_init, "cWood") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(cwood_init = myvar),
       by = c("lon", "lat")
     ) %>%
     left_join(
-      read_nc_onefile(paste0(dir, "/data/", filn_cwood_final, ".nc"), ignore_time = TRUE, varnam = "cWood") %>%
-        nc_to_df(varnam = "cWood") %>%
+      my_read_nc_onefile(dir, filn_cwood_final, "cWood") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(cwood_final = myvar),
       by = c("lon", "lat")
     ) %>%
 
-    ## NPP
+    # NPP
     left_join(
       read_nc_onefile(paste0(dir, "/data/", filn_npp_init, ".nc"), ignore_time = TRUE, varnam = "npp") %>%
         nc_to_df(varnam = "npp") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(npp_init = myvar) %>%
-        mutate(npp_init = npp_init * 31536000),
+        mutate(npp_init = npp_init * 60*60*24*365),
       by = c("lon", "lat")
     ) %>%
     left_join(
@@ -106,11 +100,11 @@ collect_gdf_bymodl <- function(
         nc_to_df(varnam = "npp") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(npp_final = myvar) %>%
-        mutate(npp_final = npp_final * 31536000),
+        mutate(npp_final = npp_final * 60*60*24*365),
       by = c("lon", "lat")
     ) %>%
 
-    ## soil C change during the last decade
+    # soil C change during the last decade
     left_join(
       read_nc_onefile(paste0(dir, "/data/", filn_csoil_change, ".nc"), ignore_time = TRUE, varnam = "cSoil") %>%
         nc_to_df(varnam = "cSoil") %>%
@@ -119,7 +113,7 @@ collect_gdf_bymodl <- function(
       by = c("lon", "lat")
     ) %>%
 
-    ## Veg C change during the last decade
+    # Veg C change during the last decade
     left_join(
       read_nc_onefile(paste0(dir, "/data/", filn_cveg_change, ".nc"), ignore_time = TRUE, varnam = "cVeg") %>%
         nc_to_df(varnam = "cVeg") %>%
@@ -127,13 +121,14 @@ collect_gdf_bymodl <- function(
         dplyr::rename(cveg_change = myvar),
       by = c("lon", "lat")
     ) %>%
-    ### GPP
+
+    # GPP
     left_join(
       read_nc_onefile(paste0(dir, "/data/", filn_gpp_init, ".nc"), ignore_time = TRUE, varnam = "gpp") %>%
         nc_to_df(varnam = "gpp") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(gpp_init = myvar) %>%
-        mutate(gpp_init = gpp_init*31536000),
+        mutate(gpp_init = gpp_init * 60*60*24*365),
       by = c("lon", "lat")
     ) %>%
     left_join(
@@ -141,27 +136,29 @@ collect_gdf_bymodl <- function(
         nc_to_df(varnam = "gpp") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(gpp_final = myvar) %>%
-        mutate(gpp_final = gpp_final*31536000),
+        mutate(gpp_final = gpp_final * 60*60*24*365),
       by = c("lon", "lat")
     ) %>%
-    ### heterotrophic respiration
-    left_join(
-      read_nc_onefile(paste0(dir, "/data/", filn_rh_init, ".nc"), ignore_time = TRUE, varnam = "rh") %>%
-        nc_to_df(varnam = "rh") %>%
-        tidyr::drop_na(myvar) %>%
-        dplyr::rename(rh_init = myvar) %>%
-        mutate(rh_init = rh_init*31536000),
-      by = c("lon", "lat")
-    ) %>%
-    left_join(
-      read_nc_onefile(paste0(dir, "/data/", filn_rh_final, ".nc"), ignore_time = TRUE, varnam = "rh") %>%
-        nc_to_df(varnam = "rh") %>%
-        tidyr::drop_na(myvar) %>%
-        dplyr::rename(rh_final = myvar) %>%
-        mutate(rh_final = rh_final*31536000),
-      by = c("lon", "lat")
-    ) %>%
-    ### cLitter
+
+    # # heterotrophic respiration
+    # left_join(
+    #   read_nc_onefile(paste0(dir, "/data/", filn_rh_init, ".nc"), ignore_time = TRUE, varnam = "rh") %>%
+    #     nc_to_df(varnam = "rh") %>%
+    #     tidyr::drop_na(myvar) %>%
+    #     dplyr::rename(rh_init = myvar) %>%
+    #     mutate(rh_init = rh_init*31536000),
+    #   by = c("lon", "lat")
+    # ) %>%
+    # left_join(
+    #   read_nc_onefile(paste0(dir, "/data/", filn_rh_final, ".nc"), ignore_time = TRUE, varnam = "rh") %>%
+    #     nc_to_df(varnam = "rh") %>%
+    #     tidyr::drop_na(myvar) %>%
+    #     dplyr::rename(rh_final = myvar) %>%
+    #     mutate(rh_final = rh_final*31536000),
+    #   by = c("lon", "lat")
+    # ) %>%
+
+    # cLitter
     left_join(
       read_nc_onefile(paste0(dir, "/data/", filn_cLitter_init, ".nc"), ignore_time = TRUE, varnam = "cLitter") %>%
         nc_to_df(varnam = "cLitter") %>%
@@ -177,16 +174,31 @@ collect_gdf_bymodl <- function(
 
       by = c("lon", "lat")
     ) %>%
+
+    # Land cover fraction
     left_join(
-      read_nc_onefile(paste0(dir, "/data/", filn_landCoverFrac, ".nc"), ignore_time = TRUE, varnam = "landCoverFrac") %>%
-        nc_to_df(varnam = "landCoverFrac") %>%
+      my_read_nc_onefile(dir, filn_landCoverFrac, "landCoverFrac") %>%
         tidyr::drop_na(myvar) %>%
         dplyr::rename(landCoverFrac = myvar),
 
       by = c("lon", "lat")
     ) %>%
-    ## add model name as column
+
+    # add model name as column
     mutate(modl = modl)
 
   return(gdf)
 }
+
+my_read_nc_onefile <- function(use_dir, use_filn, use_varnam){
+  path <- paste0(use_dir, "/data/", use_filn, ".nc")
+  if (file.exists(path)){
+    read_nc_onefile(path, ignore_time = TRUE, varnam = use_varnam) %>%
+      nc_to_df(varnam = use_varnam)
+  } else {
+    tibble(lon = NA, lat = NA, myvar = NA)
+  }
+}
+
+
+
